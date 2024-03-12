69a2779d2585a7575f67b140414a150cfe7d0bd4
ThoriqHanif <thoriqmuh123.alkapro@gmail.com>
2024-03-12 14:53:37 +0700

    update 2

--- routes/web.php
+++ routes/web.php
@@ -41,5 +41,115 @@ Route::get('/islamic', function () {
 Route::get('/forex-trading', function () {
     return view('forex-trading');
 });
+Route::get('/cryptocurrencies', function () {
+    return view('forex-trading');
+});
+Route::get('/cfd-saham', function () {
+    return view('cfd');
+});
+Route::get('/saham-turbo', function () {
+    return view('turbo-saham');
+});
+
+
+Route::get('/commodities', function () {
+    return view('commodities');
+});
+
+Route::get('/equity-indices', function () {
+    return view('equity-indices');
+});
+
+Route::get('/precious-metals', function () {
+    return view('precious-metals');
+});
+
+Route::get('/energies', function () {
+    return view('energies');
+});
+
+Route::get('/shares', function () {
+    return view('shares');
+});
+Route::get('/execution-policy', function () {
+    return view('execution-policy');
+});
+
+Route::get('/spreads', function () {
+    return view('spreads');
+});
+
+Route::get('/margin-and-leverage', function () {
+    return view('margin-and-leverage');
+});
+
+Route::get('/overnight-positions', function () {
+    return view('overnight-positions');
+});
+
+Route::get('/trading-hours', function () {
+    return view('trading-hours');
+});
+
+Route::get('/social-copy-trading', function () {
+    return view('social-copy-trading');
+});
+
+Route::get('/social-competitions', function () {
+    return view('social-competitions');
+});
+
+Route::get('/mt4', function () {
+    return view('mt4');
+});
+
+Route::get('/metatrader-mac', function () {
+    return view('metatrader-mac');
+});
+
+Route::get('/multiterminal', function () {
+    return view('multiterminal');
+});
+
+Route::get('/mt4-webtrader', function () {
+    return view('mt4-webtrader');
+});
+
+Route::get('/mt5', function () {
+    return view('mt5');
+});
+
+Route::get('/mt5-mac', function () {
+    return view('mt5-mac');
+});
+
+Route::get('/mt5-webtrader', function () {
+    return view('mt5-webtrader');
+});
+
+Route::get('/iphone', function () {
+    return view('iphone');
+});
+
+Route::get('/android', function () {
+    return view('android');
+});
+
+Route::get('/mt5-iphone', function () {
+    return view('mt5-iphone');
+});
+
+Route::get('/ipad', function () {
+    return view('ipad');
+});
+
+Route::get('/android-tablet', function () {
+    return view('android-tablet');
+});
+
+Route::get('/mt5-ipad', function () {
+    return view('mt5-ipad');
+});
+
 
 
